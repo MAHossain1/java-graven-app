13f8799347c2a716d52fa352ac2ce279b4adad87
MAHossain1 <mahpro110@gmail.com>
2025-03-07 11:26:40 +0600

    building jar and deploy to docker

--- script.groovy
+++ script.groovy
@@ -4,25 +4,25 @@ def testApp() {
 
 def buildJar() {
     echo "building the application..."
-    // sh 'mvn package'
+    sh 'mvn package'
 } 
 
 def buildImage() {
     echo "building the docker image..."
-    // withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-    //     sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
-    //     sh "echo $PASS | docker login -u $USER --password-stdin"
-    //     sh 'docker push nanajanashia/demo-app:jma-2.0'
-    // }
+    withCredentials([usernamePassword(credentialsId: 'my-dockerhub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push nanajanashia/demo-app:jma-2.0'
+    }
 }      
 
 def deployApp() {
        echo "building the docker image ${params.VERSION}"
-    //    withCredentials([usernamePassword(credentialsId: 'my-dockerhub-credentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-    //         sh 'docker build -t  arman04/java-maven-app:jma-3.0 .'
-    //         sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
-    //         sh 'docker push arman04/java-maven-app:jma-3.0'
-    // }
+       withCredentials([usernamePassword(credentialsId: 'my-dockerhub-credentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+            sh 'docker build -t  arman04/java-maven-app:jma-3.0 .'
+            sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
+            sh 'docker push arman04/java-maven-app:jma-3.0'
+    }
 } 
   
 return this
